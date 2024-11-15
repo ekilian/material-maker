d84d8c5298fe5ac3525f302bd1a1a47b1140d415
Erich Kilian <erich.kilian@gmail.com>
2024-11-15 23:23:31 +0100

    update ui

--- material_maker/panels/paint/scenes/new_project_panel.gd
+++ material_maker/panels/paint/scenes/new_project_panel.gd
@@ -1,17 +1,16 @@
 extends PanelContainer
 
-@onready var paint_emission = $Darken/VBoxContainer/Settings/VBoxContainer/Panel/Margin/Grid/PaintEmission
-@onready var paint_normal = $Darken/VBoxContainer/Settings/VBoxContainer/Panel/Margin/Grid/PaintNormal
-@onready var paint_depth = $Darken/VBoxContainer/Settings/VBoxContainer/Panel/Margin/Grid/PaintDepth
-@onready var paint_bump = $Darken/VBoxContainer/Settings/VBoxContainer/Panel/Margin/Grid/Bump/PaintBump
-@onready var paint_bump_value = $Darken/VBoxContainer/Settings/VBoxContainer/Panel/Margin/Grid/Bump/BumpStrength
-@onready var resolution = $Darken/VBoxContainer/Settings/VBoxContainer/Panel/Margin/Grid/Resolution/Resolution
+@onready var paint_emission = $Darken/VBoxContainer/Settings/Panel/VBoxContainer/Margin/Grid/PaintEmission
+@onready var paint_normal = $Darken/VBoxContainer/Settings/Panel/VBoxContainer/Margin/Grid/PaintNormal
+@onready var paint_depth = $Darken/VBoxContainer/Settings/Panel/VBoxContainer/Margin/Grid/PaintDepth
+@onready var paint_bump = $Darken/VBoxContainer/Settings/Panel/VBoxContainer/Margin/Grid/Bump/PaintBump
+@onready var paint_bump_value = $Darken/VBoxContainer/Settings/Panel/VBoxContainer/Margin/Grid/Bump/BumpStrength
+@onready var resolution = $Darken/VBoxContainer/Settings/Panel/VBoxContainer/Margin/Grid/Resolution/Resolution
 
-@onready var model_file = $Darken/VBoxContainer/Project/Panel/Margin/GridContainer/ModelFile
-@onready var mask_file = $Darken/VBoxContainer/Optionals/VBoxContainer/Panel/Margin/GridContainer/ChooseFile
-@onready var project_filename = $Darken/VBoxContainer/Project/Panel/Margin/GridContainer/ProjectName
+@onready var model_file = $Darken/VBoxContainer/Project/Panel/VBoxContainer/Margin/Grid/ModelFile
+@onready var project_filename = $Darken/VBoxContainer/Project/Panel/VBoxContainer/Margin/Grid/ProjectName
+@onready var mask_file = $Darken/VBoxContainer/Optionals/Panel/VBoxContainer/Margin/Grid/ChooseFile
 
-var _mesh_filename : String;
 
 signal project_created(project: Dictionary)
 signal model_selected(file_name:String)
@@ -24,6 +23,7 @@ func _ready() -> void:
 	paint_depth.button_pressed = true
 	paint_bump.button_pressed = true
 	paint_bump_value.value = 0.5
+	mask_file
 
 
 func _on_start_project_pressed() -> void:
@@ -55,6 +55,8 @@ func _on_choose_model_pressed():
 		model_selected.emit(model_file.text)
 		project_filename.text = model_file.text.get_file()+".mmpp"
 	await get_tree().process_frame
+	if model_file.text && project_filename.text:
+		%Start.disabled = false
 
 
 func _on_choose_color_id_file_pressed() -> void:
@@ -72,3 +74,8 @@ func _get_file_dialog(filters: Array) -> FileDialog:
 	for filter in filters:
 		file_dialog.add_filter(filter);
 	return file_dialog
+
+
+func _on_project_name_focus_exited() -> void:
+	if model_file.text && project_filename.text:
+		%Start.disabled = false
